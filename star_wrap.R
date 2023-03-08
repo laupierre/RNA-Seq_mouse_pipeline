4ad9758bf2748ef1263208ea8bd6fff529fc4217
laupierre <laupierre@users.noreply.github.com>
2023-03-08 15:06:03 +0100

    Update star_wrap.R

--- star_wrap.R
+++ star_wrap.R
@@ -7,7 +7,6 @@ system ("cp /projects/ncrrbt_share_la/dev_pipe/gencode.vM32.annotation.txt .")
 anno <- read.delim ("gencode.vM32.annotation.txt")
 anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
 anno <- unique (anno)
-head (anno)
 
 a <- read.delim ("./projects/star_results/subread.counts.txt", skip=1)
 a <- a[ ,grep ("ene|bam", colnames (a))]
@@ -19,7 +18,6 @@ a <- merge (a, anno, by.x="Geneid", by.y="gene_id", all.x=TRUE)
 a <- a[ ,grep ("gene_type.y|gene_name.y", colnames (a), invert=TRUE)]
 colnames (a) [colnames (a) == "gene_name.x"] <- "gene_name"
 colnames (a) [colnames (a) == "gene_type.x"] <- "gene_type"
-head (a)
   
 write.xlsx (a, "./output/star_gene_raw_counts.xlsx", rowNames=F)
 #write.table (a, "./output/star_gene_raw_counts.txt", sep="\t", quote=F, row.names=F)
