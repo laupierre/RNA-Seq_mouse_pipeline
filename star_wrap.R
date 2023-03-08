a80b053348dcb4a5abeff95a7f13521d577584c4
laupierre <laupierre@users.noreply.github.com>
2023-03-08 15:04:19 +0100

    Update star_wrap.R

--- star_wrap.R
+++ star_wrap.R
@@ -2,7 +2,12 @@ library (openxlsx)
 
 system ("mkdir ./output")
 system ("cp ./projects/log.out ./output")
-system ("cp /projects/ncrrbt_share_la/dev_pipe/ .")
+system ("cp /projects/ncrrbt_share_la/dev_pipe/gencode.vM32.annotation.txt .")
+
+anno <- read.delim ("gencode.vM32.annotation.txt")
+anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
+anno <- unique (anno)
+head (anno)
 
 a <- read.delim ("./projects/star_results/subread.counts.txt", skip=1)
 a <- a[ ,grep ("ene|bam", colnames (a))]
@@ -10,7 +15,11 @@ a <- a[grep ("miRNA|Mt_tRNA|Mt_rRNA|rRNA|snRNA|snoRNA|scRNA|sRNA|misc_RNA|scaRNA
 colnames (a) <- gsub ("_S[0-9]+.*", "", colnames (a)) 
 colnames (a) <- gsub (".*IIT_", "", colnames (a))
 
-a <- merge (a, 
+a <- merge (a, anno, by.x="Geneid", by.y="gene_id", all.x=TRUE) 
+a <- a[ ,grep ("gene_type.y|gene_name.y", colnames (a), invert=TRUE)]
+colnames (a) [colnames (a) == "gene_name.x"] <- "gene_name"
 
+head (a)
+  
 write.xlsx (a, "./output/star_gene_raw_counts.xlsx", rowNames=F)
 #write.table (a, "./output/star_gene_raw_counts.txt", sep="\t", quote=F, row.names=F)
