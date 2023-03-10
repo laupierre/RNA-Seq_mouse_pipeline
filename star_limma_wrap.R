52cb4e7cd6fbaa3f2c7032a34d5d8277323d635e
laupierre <laupierre@users.noreply.github.com>
2023-03-10 10:39:44 +0100

    Update star_limma_wrap.R

--- star_limma_wrap.R
+++ star_limma_wrap.R
@@ -32,7 +32,7 @@ sampleTable$replicate <- factor (sampleTable$replicate)
 stopifnot (sampleTable$sample == colnames (a))
 
 
-
+## with contrast
 y <- DGEList(a)
 
 # filtering using the design information:
@@ -41,6 +41,7 @@ colnames(design) <- gsub("condition", "", colnames(design))
 keep <- filterByExpr(y, design)
 y <- y[keep, ]
 
+# Manual contrast
 contr.matrix <- makeContrasts(contrast1= treated - control, levels = colnames(design))
   
 # normalize and run voom transformation
@@ -51,6 +52,56 @@ vfit <- lmFit(v, design)
 vfit <- contrasts.fit(vfit, contrasts=contr.matrix) 
 efit <- eBayes(vfit)
 res <- topTable(efit, coef=1, n=Inf)
+
+res <- merge (res, v$E, by="row.names")
+res <- merge (res, annot, by.x="Row.names", by.y="Geneid")
+colnames (res)[1] <- "Geneid"
+res1 <- res
+write.xlsx (res1, "star_limma_differential_expression.xlsx", rowNames=F)
+
+
+
+## without contrast
+y <- DGEList(a)
+
+# filtering using the design information:
+design <- model.matrix(~condition, data = sampleTable)
+colnames(design) <- gsub("condition", "", colnames(design))
+keep <- filterByExpr(y, design)
+y <- y[keep, ]
+
+
+y <- calcNormFactors(y)
+v <- voom(y, design)
+
+vfit <- lmFit(v, design)
+efit <- eBayes(vfit)
+res <- topTable(efit, n=Inf)
+
+res <- merge (res, v$E, by="row.names")
+res <- merge (res, annot, by.x="Row.names", by.y="Geneid")
+colnames (res)[1] <- "Geneid"
+
+
+
+res2 <- merge (res1, res, by="Geneid")
+table (round (res2$logFC.x, digits=4) == round (res2$logFC.y, digits=4))
+# All TRUE
+table (round(res2$adj.P.Val.x, digits=4) == round (res2$adj.P.Val.y, digits=4))
+# All TRUE
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
