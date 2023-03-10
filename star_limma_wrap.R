799875d1b29d7a7edcac33d219bc0e969a8ab17f
laupierre <laupierre@users.noreply.github.com>
2023-03-10 10:08:00 +0100

    Update star_limma_wrap.R

--- star_limma_wrap.R
+++ star_limma_wrap.R
@@ -1,8 +1,6 @@
 library (openxlsx)
-library (DESeq2)
-library (ggplot2)
 library (limma)
-
+library (edgeR)
 
 a <- annot <- read.xlsx ("./output/star_gene_raw_counts.xlsx")
 
@@ -26,6 +24,28 @@ sampleTable$condition <- factor (sampleTable$condition)
 sampleTable$replicate <- factor (sampleTable$replicate)
 
 stopifnot (sampleTable$sample == colnames (a))
+
+
+
+y <- DGEList(a)
+
+# filtering using the design information:
+design <- model.matrix(~0+condition, data = sampleTable)
+colnames(design) <- gsub("condition", "", colnames(design))
+keep <- filterByExpr(y, design)
+y <- y[keep, ]
+
+contr.matrix <- makeContrasts(contrast1= treated - control, levels = colnames(design))
+  
+# normalize and run voom transformation
+y <- calcNormFactors(y)
+v <- voom(y, design)
+
+vfit <- lmFit(v, design)
+vfit <- contrasts.fit(vfit, contrasts=contr.matrix) 
+efit <- eBayes(vfit)
+res <- topTable(efit, coef=1, n=Inf)
+
 
 
 
