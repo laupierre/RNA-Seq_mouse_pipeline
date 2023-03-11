3e784ef3162167aa85f3dd6072fb5690180f7d6a
laupierre <laupierre@users.noreply.github.com>
2023-03-11 23:52:22 +0100

    Update rnaseq_mouse_v0.0.2.sh

--- rnaseq_mouse_v0.0.2.sh
+++ rnaseq_mouse_v0.0.2.sh
@@ -216,7 +216,7 @@ var=(`ls *_R1*.fastq.gz`)
 
 	for i in ${var[@]}
 	do
-	read2=`echo ${i} | sed 's/R1/R2/g'`
+	read2=`echo ${i} | sed 's/_R1/_R2/g'`
 	prefix=`echo ${i%%_R1*}`
 
 	apptainer exec $CONTAINER/kallisto.sif /bin/bash -c \
